0c45e6fdac1d8167a9990728ea60bead6f7b7fe9
Jorge Orjuela <111378570+jorge-jcrew@users.noreply.github.com>
2023-09-12 09:29:54 -0500

    Add source code for testing

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shared-Package
+
+A description of this package.
---
+++ Tests/Shared-PackageTests/Shared_PackageTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Shared_Package
+
+final class Shared_PackageTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Shared_Package().text, "Hello, World!")
+    }
+}
